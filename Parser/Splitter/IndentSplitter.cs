80ee63d69ca19556b845f237d43193db092ad059
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2022-10-14 14:31:37 +0300

    indent splitter stub

---
+++ Parser/Splitter/IndentSplitter.cs
@@ -0,0 +1,23 @@
+using System;
+using resume_mixer.Models.Parser;
+using Microsoft.Extensions.Options;
+using resume_mixer.Config;
+using System.Collections.Generic;
+using resume_mixer.Parser.Splitter.Interface;
+
+namespace resume_mixer.Parser.Splitter
+{
+    public class IndentSplitter : IIndentSplitter
+    {
+        readonly AppConfig options;
+        public IndentSplitter(IOptions<AppConfig> o)
+        {
+            options = o.Value;
+        }
+
+        public List<string> Split(string text)
+        {
+            return null;
+        }
+    }
+}
