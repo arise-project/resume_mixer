c40607468926b9ef8bf268faf9d999de383d25e2
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2022-11-05 22:37:19 +0200

    repo stub

---
+++ Repository/DescriptionRepository.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace resume_mixer.Models.Parser
+{
+    public class DescriptionRepository : IDescriptionRepository
+    {
+    }
+}
---
+++ Repository/Interface/IKeywordRepository.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace resume_mixer.Models.Parser
+{
+    public interface IKeywordRepository : IEntityRepository
+    {
+    }
+}
